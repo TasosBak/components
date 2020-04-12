a577373f31398aadd43a3de57eb4c68d377b247a
Vakirtzis Anastasios <anastasiosvakirtzis@gmail.com>
2020-04-12 19:42:47 +0300

    implementing new component witch takes another component as prop

--- src/index.js
+++ src/index.js
@@ -1,30 +1,40 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import CommentDetail from "./CommentsDetail";
+import ApprovalCard from "./ApprovalCard";
 import faker from "faker";
 
 const App = () => {
   return (
     <div className="ui container comments">
       <h3 className="ui dividing header">Comments</h3>
-      <CommentDetail
-        author="Jane"
-        timeAgo="Today at 05:03 AM"
-        commentText={faker.lorem.sentence()}
-        avatar={faker.image.avatar()}
-      />
-      <CommentDetail
-        author="Tarzan"
-        timeAgo="Today at 06:03 AM"
-        commentText={faker.lorem.sentence()}
-        avatar={faker.image.avatar()}
-      />
-      <CommentDetail
-        author="Bill"
-        timeAgo="Today at 05:06 PM"
-        commentText={faker.lorem.sentence()}
-        avatar={faker.image.avatar()}
-      />
+
+      <ApprovalCard>
+        <CommentDetail
+          author="Jane"
+          timeAgo="Today at 05:03 AM"
+          commentText={faker.lorem.sentence()}
+          avatar={faker.image.avatar()}
+        />
+      </ApprovalCard>
+
+      <ApprovalCard>
+        <CommentDetail
+          author="Tarzan"
+          timeAgo="Today at 06:03 AM"
+          commentText={faker.lorem.sentence()}
+          avatar={faker.image.avatar()}
+        />
+      </ApprovalCard>
+
+      <ApprovalCard>
+        <CommentDetail
+          author="Bill"
+          timeAgo="Today at 05:06 PM"
+          commentText={faker.lorem.sentence()}
+          avatar={faker.image.avatar()}
+        />
+      </ApprovalCard>
     </div>
   );
 };
